be854455f7f954811bd8289513d0b2ab8290e4b6
Zahidul Hossain <choyan.009@gmail.com>
2023-03-05 22:30:10 +0600

    add umami

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script async defer data-website-id="87818b59-d080-4779-aa25-d9c521dc4a3f" src="https://umami-analytics-redeploy.vercel.app/umami.js" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
